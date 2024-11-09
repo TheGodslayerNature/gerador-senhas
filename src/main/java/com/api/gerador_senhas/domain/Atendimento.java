dc94bf501063b36ea6c79dd4c1fdf61a1aac369b
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-11-09 19:24:23 -0300

    Criando o model do atendimento.

---
+++ src/main/java/com/api/gerador_senhas/domain/Atendimento.java
@@ -0,0 +1,27 @@
+package com.api.gerador_senhas.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Atendimento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "ATENDIMENTO_ID")
+    private UUID atendimentoId;
+
+    private LocalDateTime inicio;
+
+    private LocalDateTime fim;
+
+    private String observacao;
+}
