73ea038ed5701420ed63f9ff19ce189d2d0fd0e8
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-11-12 16:01:24 -0300

    start ignoring application-dev

---
+++ src/main/java/com/api/gerador_senhas/dtos/atendimentoDtos/PostAtendimentosDto.java
@@ -0,0 +1,9 @@
+package com.api.gerador_senhas.dtos.atendimentoDtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+public record PostAtendimentosDto() {
+}
